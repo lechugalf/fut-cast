bfd86618b7bd8c519796460c2bc399a19273db67
Victor Lechuga <=>
2025-11-21 15:37:32 -0600

    fix startup script

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running migrations..."
-npx typeorm-ts-node-commonjs migration:run -d dist/src/typeorm.config.js
+npx typeorm-ts-node-commonjs migration:run -d dist/typeorm.config.js
 
 echo "Starting application..."
 node dist/main
